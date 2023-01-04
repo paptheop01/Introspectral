c8a22ea91f91d31cf9e5fda6be63de20487caffc
Chrysa Pratikaki <chrisaprat@gmail.com>
2023-01-04 23:39:34 +0200

    fakelos screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/src/painting/box_decoration.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,16 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         canvasColor: Color.fromARGB(255, 241, 186, 255),
         primarySwatch: Colors.blue,
       ),
