d703909612dcf65111ef72c9ffe1f7f22d1f64e1
Chrysa Pratikaki <chrisaprat@gmail.com>
2023-01-09 18:14:14 +0200

    bottomnavbar

--- lib/habit.dart
+++ lib/habit.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 
 import 'package:flutter/material.dart';
@@ -7,65 +6,54 @@ import 'package:introspectral/habitadd.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart' as p;
 
-
-
-
 class HabitListScreenWidget extends StatefulWidget {
-  const HabitListScreenWidget({Key?key}) : super(key:key);
+  const HabitListScreenWidget({Key? key}) : super(key: key);
 
-  
   @override
   _HabitListScreenWidgetState createState() => _HabitListScreenWidgetState();
 }
 
 class _HabitListScreenWidgetState extends State<HabitListScreenWidget> {
-  
   late SQLservice sqLiteservice;
-  List<Habit> _habits=<Habit>[];
-  void _addNewHabit() async{
-     Habit? newHabit= await Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => ViewEditHabitWidget() ));
-      if(newHabit!=null){
-        final newId=await sqLiteservice.addHabit(newHabit);
-        newHabit.id=newId;
+  List<Habit> _habits = <Habit>[];
+  void _addNewHabit() async {
+    Habit? newHabit = await Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => ViewEditHabitWidget()));
+    if (newHabit != null) {
+      final newId = await sqLiteservice.addHabit(newHabit);
+      newHabit.id = newId;
 
-        _habits.add(newHabit);
-        setState(() {
-          
-        });
-
-      }
-
+      _habits.add(newHabit);
+      setState(() {});
+    }
   }
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    sqLiteservice=SQLservice();
-    sqLiteservice.initDB().whenComplete(() async{
-      final habits= await sqLiteservice.getHabits();
+    sqLiteservice = SQLservice();
+    sqLiteservice.initDB().whenComplete(() async {
+      final habits = await sqLiteservice.getHabits();
       setState(() {
-        _habits=habits;
+        _habits = habits;
       });
     });
-    
   }
-  
 
   Widget _buildHabitList() {
     return ListView.separated(
       padding: const EdgeInsets.all(8.0),
-      itemBuilder: (context, index)  {
+      itemBuilder: (context, index) {
         IconData iconData;
         String toolTip;
         TextDecoration textDEc;
 
-        iconData=  Icons.check_box_outline_blank_outlined;
-        toolTip=  'Mark as completed';
-        textDEc=  TextDecoration.none;
-        
-        return Stack(
-          
-          children:[/*Center(child:Container(
+        iconData = Icons.check_box_outline_blank_outlined;
+        toolTip = 'Mark as completed';
+        textDEc = TextDecoration.none;
+
+        return Stack(children: [
+          /*Center(child:Container(
               height: 50,
                 decoration: BoxDecoration(
 
@@ -73,66 +61,56 @@ class _HabitListScreenWidgetState extends State<HabitListScreenWidget> {
                   color: Colors.red,
                 ),
               )),*/
-             Dismissible(
-              background:Container(
-               
+          Dismissible(
+            background: Container(
               height: 50,
-                decoration: BoxDecoration(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.blue,
+              ),
+            ),
+            key: UniqueKey(),
+            direction: DismissDirection.startToEnd,
+            onDismissed: (direction) {
+              // Remove the item from the list
 
-                  borderRadius:BorderRadius.circular(25) ,
-                  color: Colors.blue,) ,),
-              key: UniqueKey() ,  
-                
-              
-              direction: DismissDirection.startToEnd,
-              onDismissed: (direction) {
-                // Remove the item from the list
-               
-                _deleteHabit(index);
-              },
-              
-              child: Stack(children: [Container(
-                height: 50,
-                
-                decoration: BoxDecoration(
-                  color: Color(0x3300FF19),
-                  borderRadius: BorderRadius.circular(25),
-                )),
-               
-                Container(
-                height: 50,
-                width: 200,
-                decoration: BoxDecoration(
-                  color: Color(0xFF00FF19),
-                  borderRadius: BorderRadius.circular(25),
-                )),
-                 Container( 
-                  decoration: BoxDecoration(
-                  color: Colors.transparent,
-                  borderRadius: BorderRadius.circular(25),
-                ),
+              _deleteHabit(index);
+            },
+            child: Stack(children: [
+              Container(
                   height: 50,
-                  padding: EdgeInsets.symmetric(vertical: 12.0,horizontal: 8.0),
-                  child: Text(_habits[index].title,
+                  decoration: BoxDecoration(
+                    color: Color(0x3300FF19),
+                    borderRadius: BorderRadius.circular(25),
+                  )),
+              Container(
+                  height: 50,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF00FF19),
+                    borderRadius: BorderRadius.circular(25),
+                  )),
+              Container(
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  height: 50,
+                  padding:
+                      EdgeInsets.symmetric(vertical: 12.0, horizontal: 8.0),
+                  child: Text(
+                    _habits[index].title,
                     style: TextStyle(
                       color: Colors.black,
                       fontSize: 16,
                     ),
-                  )
-                ),
-                ]),
-                 
-              )
-              
-          ]
-          );
-              },
-    
-        
-        
-        
-        
-        /*return ListTile(
+                  )),
+            ]),
+          )
+        ]);
+      },
+
+      /*return ListTile(
           
 
           leading: IconButton(
@@ -159,13 +137,14 @@ class _HabitListScreenWidgetState extends State<HabitListScreenWidget> {
         
         );},
         */
-      separatorBuilder: (context, index) =>const Divider(),
+      separatorBuilder: (context, index) => const Divider(),
       itemCount: _habits.length,
     );
   }
 
-  void _deleteHabit(int idx) async{
-    bool? delHabit =true;/* await showDialog<bool>(
+  void _deleteHabit(int idx) async {
+    bool? delHabit =
+        true; /* await showDialog<bool>(
       context: context, 
       builder: (BuildContext context) => AlertDialog(
         content: const Text('Delete Habit?'),
@@ -176,49 +155,78 @@ class _HabitListScreenWidgetState extends State<HabitListScreenWidget> {
           child: const Text('delete')),
         ],
       ));*/
-    if(delHabit){
-      final habit=_habits.elementAt(idx);
-      try{
-        
+    if (delHabit) {
+      final habit = _habits.elementAt(idx);
+      try {
         sqLiteservice.deleteHabit(habit.id);
-        
+
         _habits.removeAt(idx);
-        
       } catch (err) {
-          debugPrint('Could not delete habit $habit : $err');
+        debugPrint('Could not delete habit $habit : $err');
       }
-      setState(() {
-        
-      });
-
-    }  
-
+      setState(() {});
+    }
   }
-  
+
+  int _selectedIndex = 1;
+  void _onItemTapped(int index) {
+    switch (index) {
+      case 0:
+        break;
+      case 1:
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => HabitListScreenWidget()));
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      body : Stack(
-      children: <Widget>[
-          Container(
-          decoration:  BoxDecoration(
-            image:  DecorationImage(image:  AssetImage("assets/images/back.png"), fit: BoxFit.cover,),
+      body: Stack(children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/back.png"),
+              fit: BoxFit.cover,
+            ),
           ),
-        ), 
+        ),
         _buildHabitList(),
-        ]
-        ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            IconButton(onPressed: () {}, icon: const Icon(null))
-          ],
-        ),
-        color: Colors.blue,),
+      ]),
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Color.fromARGB(61, 0, 0, 0),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Habits',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.note),
+            label: 'Log',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.show_chart),
+            label: 'Stats',
+          ),
+        ],
+        currentIndex: 0,
+        onTap: _onItemTapped,
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: _addNewHabit,
@@ -228,81 +236,76 @@ class _HabitListScreenWidgetState extends State<HabitListScreenWidget> {
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
-  
 }
-
 
 class Habit {
   int? id;
   String title;
   int completed;
   int goal;
-  Habit({
-    this.id,
-    required this.title, 
-    required this.goal,
-    required this.completed }); 
+  Habit(
+      {this.id,
+      required this.title,
+      required this.goal,
+      required this.completed});
 
+  Map<String, dynamic> toMap() {
+    final record = {'title': title, 'completed': completed, 'goal': goal};
 
-  Map<String,dynamic> toMap(){
-    final record={'title':title,'completed':completed,'goal':goal};
-    
     return record;
-
   }
-  Habit.fromMap(Map<String,dynamic> habit):
-    id=habit['id'],
-    title=habit['title'],
-    completed= habit['completed'],
-    goal=habit['goal'];
 
-
-
+  Habit.fromMap(Map<String, dynamic> habit)
+      : id = habit['id'],
+        title = habit['title'],
+        completed = habit['completed'],
+        goal = habit['goal'];
 }
 
-
-
-class SQLservice{
-  Future <Database> initDB() async{
+class SQLservice {
+  Future<Database> initDB() async {
     return openDatabase(
       p.join(await getDatabasesPath(), 'habits.db'),
-      onCreate:(db, version) {
+      onCreate: (db, version) {
         return db.execute(
-          'CREATE TABLE habits(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed INTEGER,goal INTEGER)'
-        );
+            'CREATE TABLE habits(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed INTEGER,goal INTEGER)');
       },
-      version: 1, 
+      version: 1,
     );
-  
   }
-  Future <List<Habit>> getHabits() async{
-    final db=await initDB();
-    final List<Map<String,Object?>> queryResult= await db.query('habits');
+
+  Future<List<Habit>> getHabits() async {
+    final db = await initDB();
+    final List<Map<String, Object?>> queryResult = await db.query('habits');
     return queryResult.map((e) => Habit.fromMap(e)).toList();
   }
-  Future<int> addHabit(Habit habit) async{
-    final db= await initDB();
+
+  Future<int> addHabit(Habit habit) async {
+    final db = await initDB();
     return db.insert('habits', habit.toMap(),
-    conflictAlgorithm: ConflictAlgorithm.replace);
+        conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
-  Future <void> deleteHabit(final id) async{
-    final db= await initDB();
-    await db.delete('habits',where: 'id=?',whereArgs: [id]);
+  Future<void> deleteHabit(final id) async {
+    final db = await initDB();
+    await db.delete('habits', where: 'id=?', whereArgs: [id]);
   }
 
+  Future<void> updateComplete(Habit habit) async {
+    final db = await initDB();
+    await db.update('habits', {'completed': habit.completed},
+        where: 'id=?',
+        whereArgs: [habit.id],
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
 
-  Future <void> updateComplete(Habit habit) async{
-    final db= await initDB();
-    await db.update('habits',{'completed':habit.completed},where: 'id=?',whereArgs: [habit.id],conflictAlgorithm: ConflictAlgorithm.replace);
+  Future<void> deleteCompleted() async {
+    final db = await initDB();
+    await db.delete('habits', where: 'completed=1');
   }
-  Future <void> deleteCompleted() async{
-    final db= await initDB();
-    await db.delete('habits',where: 'completed=1');
-  }
-  Future <void> deleteAllHabits() async{
-    final db= await initDB();
+
+  Future<void> deleteAllHabits() async {
+    final db = await initDB();
     await db.delete('habits');
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -25,49 +25,82 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class HomeScreenWidget extends StatefulWidget {
-  const HomeScreenWidget({Key?key}) : super(key:key);
+  const HomeScreenWidget({Key? key}) : super(key: key);
 
-  
   @override
   _HomeScreenWidgetState createState() => _HomeScreenWidgetState();
 }
 
-
 class _HomeScreenWidgetState extends State<HomeScreenWidget> {
-  void _gotohabits(){
-    Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => HabitListScreenWidget() ));
+  void _gotohabits() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => HabitListScreenWidget()));
   }
 
-
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    switch (index) {
+      case 0:
+        break;
+      case 1:
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => HabitListScreenWidget()));
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-    body: new Stack(
-      children: <Widget>[
-        new Container(
-          decoration: new BoxDecoration(
-            image: new DecorationImage(image: new AssetImage("assets/images/back.png"), fit: BoxFit.cover,),
+      body: new Stack(
+        children: <Widget>[
+          new Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage("assets/images/back.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-        ),
-        new Center(
-          child: new Text("Hello background"),
-        )
-      ],
-    ),
-    floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: _gotohabits,
-        backgroundColor: Colors.teal,
-        tooltip: 'Add Task',
+          new Center(
+            child: new Text("Hello background"),
+          )
+        ],
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Color.fromARGB(61, 0, 0, 0),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Habits',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.note),
+            label: 'Log',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.show_chart),
+            label: 'Stats',
+          ),
+        ],
+        currentIndex: 0,
+        onTap: _onItemTapped,
+      ),
     );
-    }
-  
+  }
 }
-
-
